2bc0ef0c8d0a438d26bc8f10c036b8ab5603dfa3
Rob <kaz.brah@gmail.com>
2020-03-11 16:19:00 -0400

    Updating output to work with c# code

--- Main.py
+++ Main.py
@@ -17,12 +17,20 @@ if os.path.exists(os.getcwd() +'\\' + fn +'.xlsx'):  # file was found
         test_d = sh.cell(i, 3).value  # load value from cell (i,0) i row first column
         step_d = sh.row_values(i, 6, 7)  # load values i row second column
         result_d = sh.row_values(i, 7, 8)  # load values i row third column
-        DB1 = test_d + " " + (" ".join(step_d))  # build test step portion
-        DB2 = " ".join(result_d)  # build test result portion
-        my_file.write(DB1 + ' | ' + DB2 + "\n")  # write data to file
-        #my_file.write(step_d)
+        if test_d == '':
+            DB1 = " ".join(step_d)  # build test step portion
+            DB2 = " ".join(result_d)  # build test result portion
+            if DB1 == '':
+                my_file.write("\n")  # print line between last step and next test name
+            else:
+                my_file.write('|' + DB1 + '|' + DB2 + "\n")  # write data to file
+        else:
+            DB1 = "[" + test_d + "]\n"
+            my_file.write(DB1)  # write text name
         i += 1  # increment i for next row
         # print("Line " + str(i) + " was written.")  # console log of progress
+    eof = "[eof]\n"
+    my_file.write(eof)  # print [eof] at end for later parsing
     my_file.close()  # release resources
     print("All done. " + str(i) + " lines written.")
 else:  # file not found
